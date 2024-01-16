31ae47aebad607083cff36e9e3baa5c91f70476d
Joseph Gaughan <jojopotato9393@gmail.com>
2024-01-15 19:30:34 -0600

    first exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            
+                var r = new Random();
+            var faveNumber = r.Next(1,10);
+            Console.WriteLine("Try to guess my favorite number");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < faveNumber)
+            {
+                Console.WriteLine("to low");
+            }
+            else if (userInput > faveNumber)
+            {
+                Console.WriteLine("to high");
+
+            }
+            if (userInput == faveNumber)
+            {
+                Console.WriteLine("Good job, you got it");
+            }
+
+            
+          
         }
     }
 }
