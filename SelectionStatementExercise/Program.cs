9107d635ab53d5fb56eaf6b3ed8bf36d33aba6ba
Joseph Gaughan <jojopotato9393@gmail.com>
2024-01-15 20:27:15 -0600

    exercise1-2

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,12 +1,15 @@
-﻿namespace SelectionStatementExercise
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            
-            
-                var r = new Random();
+
+         
+
+            var r = new Random();
             var faveNumber = r.Next(1,10);
             Console.WriteLine("Try to guess my favorite number");
             var userInput = int.Parse(Console.ReadLine());
@@ -25,8 +28,40 @@
                 Console.WriteLine("Good job, you got it");
             }
 
-            
-          
+
+            Console.WriteLine("what is your favorite subject: ");
+
+            string subject = Console.ReadLine();
+           
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("great choice math is very importent");            
+                    break;
+
+                case "english":
+                    Console.WriteLine("good choice very useful in many jobs");
+                    break;
+                case "science":
+                    Console.WriteLine("very good choice");
+                    break;
+                case "hisstory":
+                    Console.WriteLine("very interesting ");
+                    break;
+                case "art":
+                    Console.WriteLine("good choice");
+                    break;
+                default:
+                    Console.WriteLine("i dont know about that onn..");
+                    break;             
+
+            }
+
+
         }
+    
+
+
     }
 }
